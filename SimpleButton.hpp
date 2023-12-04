df86b273125a7257789a846c1756b58e120aafbe
LoveGood <7620678+Luna-LoveGood@user.noreply.gitee.com>
2023-12-04 21:36:24 +0800

    solve btn bug

--- SimpleButton.hpp
+++ SimpleButton.hpp
@@ -18,158 +18,120 @@
 #include <QPushButton>
 
 
-
 class CSimpleButton : public  QPushButton
 {
  public:
+	CSimpleButton::CSimpleButton()
+	{
+		text = "Hold That";
+		full_color = QColor("#00A97F");
+		border_radius = 5;
+		timer_interval = 8;
+		font_anim_finish_color = QColor("#ffffff");
+		font_anim_start_color = QColor("#00A97F");
 
-	 CSimpleButton::CSimpleButton()
-	 {
-		 text = "Hold That";
-		 full_color = QColor("#00A97F");
-		 border_radius = 5;
-		 timer_interval = 8;
-		 font_anim_finish_color = QColor("#ffffff");
-		 font_anim_start_color = QColor("#00A97F");
+		font_color = full_color.name();
+		QString border_color = full_color.name();
+		setObjectName("SimpleButton");
+		setStyleSheet(QString("CSimpleButton#SimpleButton {"
+			"    border:1px solid %1;"
+			"    background-color: rgba(0, 0, 0,0);"
+			"    border-radius:%2px;"
+			"}").arg(border_color).arg(border_radius));
 
-		 font_color = full_color.name();
-		 QString border_color = full_color.name();
-		 setObjectName("SimpleButton");
-		 setStyleSheet(QString("CSimpleButton#SimpleButton {"
-			 "    border:1px solid %1;"
-			 "    background-color: rgba(0, 0, 0,0);"
-			 "    border-radius:%2px;"
-			 "}").arg(border_color).arg(border_radius));
-	 }
+		animationConfig(); // 将一些设置打包
+	}
 
-	 CSimpleButton::~CSimpleButton() {
- 
-	 }
+	CSimpleButton::~CSimpleButton() {}
 
- 
+	void CSimpleButton::animationConfig() {
+		radius = 0;
+		radiusVar = 3;
+		max_radius = sqrt(pow(width(), 2) + pow(height(), 2));
+		if (timer != nullptr) {
+			timer->stop();
+			timer->start();
+		}
+		timer->setInterval(timer_interval);
 
-	 void CSimpleButton::setParam(std::string str, QColor full_color_, QColor font_anim_start_color_, QColor font_anim_finish_color_, int border_radius_, int timer_interval_)
-	 {
-		 text = QString::fromStdString(str);
-		 full_color = full_color_;
-		 border_radius = border_radius_;
-		 timer_interval = timer_interval_;
-		 font_anim_finish_color = font_anim_finish_color_;
-		 font_anim_start_color = font_anim_start_color_;
+	}
 
-		 font_color = full_color.name();
+	// 修改半径:增加
+	void CSimpleButton::incRadius() {
+		radius += radiusVar;
+		if (radius > max_radius) {
+			timer->stop();
+		}
+		update();
+	}
 
+	// 修改半径：减少
+	void CSimpleButton::decRadius() {
+		radius -= radiusVar;
+		if (radius < 0) {
+			timer->stop();
+			font_color = font_anim_start_color;
+		}
+		update();
+	}
 
-		 QString border_color = full_color.name();
-		 setObjectName("SimpleButton_1");
-		 setStyleSheet(QString("CSimpleButton#SimpleButton_1 {"
-			 "    border:1px solid %1;"
-			 "    background-color: rgba(0, 0, 0,0);"
-			 "    border-radius:%2px;"
-			 "}").arg(border_color).arg(border_radius));
-	 }
+	void CSimpleButton::setText(const QString& text) {
+		this->text = text;
+	}
 
+	void CSimpleButton::paintText() {
+		/* 绘制文字 */
+		QPainterPath path;
+		path.addRoundedRect(0, 0, width(), height(), border_radius, border_radius);
 
-	 void CSimpleButton::animationConfig() {
-		 radius = 0;
-		 radiusVar = 3;
-		 max_radius = sqrt(pow(width(), 2) + pow(height(), 2));
+		QPainter painter(this);
+		painter.setPen(Qt::NoPen);
+		painter.setClipPath(path);
+		painter.setFont(font());
+		painter.setPen(QColor(font_color));
+		painter.setRenderHint(QPainter::Antialiasing);
 
-		 if (timer != nullptr) {
-			 timer->stop();
-			 timer->start();
-		 }
-		 //timer = new QTimer(this);
-		 timer->setInterval(timer_interval);
-		 connect(timer, &QTimer::timeout, this, &CSimpleButton::incRadius);
+		if (!text.isEmpty()) {
+			painter.drawText(rect(), Qt::AlignCenter, text);
+		}
+	}
 
-	 }
+	void CSimpleButton::paintEvent(QPaintEvent* event) {
+		QPushButton::paintEvent(event);
 
-	 // 修改半径
-	 void CSimpleButton::incRadius() {
-		 radius += radiusVar;
-		 if (radius > max_radius) {
-			 timer->stop();
-			 return;
-		 }
-		 update();
-	 }
+		QPainter painter(this);
+		painter.setRenderHint(QPainter::Antialiasing);
 
-	 // 修改半径
-	 void CSimpleButton::decRadius() {
-		 radius -= radiusVar;
-		 if (radius < 0) {
-			 timer->stop();
-			 return;
-		 }
-		 update();
-	 }
+		QBrush brush(full_color);
+		painter.setBrush(brush);
+		painter.setPen(Qt::NoPen);
 
-	 void CSimpleButton::setText(const QString& text) {
-		 this->text = text;
-	 }
+		QPainterPath path;
+		path.addRoundedRect(rect(), border_radius, border_radius);
+		painter.setClipPath(path);
 
- 
-	 void CSimpleButton::paintText() {
-		 /* 绘制文字 */
-		 QPainterPath path;
-		 path.addRoundedRect(0, 0, width(), height(), border_radius, border_radius);
+		painter.drawEllipse(QPoint(0, 0), radius, radius);
+		painter.drawEllipse(QPoint(width(), height()), radius, radius);
 
-		 QPainter painter(this);
-		 painter.setPen(Qt::NoPen);
-		 painter.setClipPath(path);
-		 painter.setFont(font());
-		 painter.setPen(QColor(font_color));
-		 painter.setRenderHint(QPainter::Antialiasing);
+		paintText();
+	}
 
-		 if (!text.isEmpty()) {
-			 painter.drawText(rect(), Qt::AlignCenter, text);
-		 }
-	 }
+	void CSimpleButton::enterEvent(QEvent* event) {
+		font_color = font_anim_finish_color;
+		timer->disconnect();
+		connect(timer, &QTimer::timeout, this, &CSimpleButton::incRadius);
+		timer->start();
+	}
 
-	 void CSimpleButton::paintEvent(QPaintEvent* event) {
-		 QPushButton::paintEvent(event);
+	void CSimpleButton::leaveEvent(QEvent* event) {
+		timer->disconnect();
+		connect(timer, &QTimer::timeout, this, &CSimpleButton::decRadius);
+		timer->start();
+	}
 
-		 QPainter painter(this);
-		 painter.setRenderHint(QPainter::Antialiasing);
-
-		 QBrush brush(full_color);
-		 painter.setBrush(brush);
-		 painter.setPen(Qt::NoPen);
-
-		 QPainterPath path;
-		 path.addRoundedRect(rect(), border_radius, border_radius);
-		 painter.setClipPath(path);
-
-		 painter.drawEllipse(QPoint(0, 0), radius, radius);
-		 painter.drawEllipse(QPoint(width(), height()), radius, radius);
-
-		 paintText();
-	 }
-
-	 void CSimpleButton::enterEvent(QEvent* event) {
-		 font_color = font_anim_finish_color;
-		 timer->disconnect();
-		 connect(timer, &QTimer::timeout, this, &CSimpleButton::incRadius);
-		 timer->start();
-	 }
-
-	 void CSimpleButton::leaveEvent(QEvent* event) {
-		 font_color = font_anim_finish_color;
-		 timer->disconnect();
-		 connect(timer, &QTimer::timeout, this, &CSimpleButton::decRadius);
-		 timer->start();
-	 }
-
-	 void CSimpleButton::showEvent(QShowEvent* event) {
-		 QPushButton::showEvent(event);
-		 animationConfig();
-	 }
-
-
-
-
-
+	void CSimpleButton::showEvent(QShowEvent* event) {
+		QPushButton::showEvent(event);
+	}
 
 
 public:
@@ -180,18 +142,13 @@ public:
 	int border_radius;
 	int timer_interval;
 
-
-
 	QString text;
 	QColor full_color;
 	QColor font_color;
 	QColor font_anim_start_color;
 	QColor font_anim_finish_color;
 
-	//std::unique_ptr<QTimer> timer;
-
 	QTimer* timer =   new QTimer(this);
-	;
 };
 
  
